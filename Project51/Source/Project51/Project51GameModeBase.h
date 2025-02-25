fe1749b04323d7125f3bdebdbfef7985b2d2d8e0
vimrzhang <vimrzhang@tencent.com>
2025-02-25 16:34:45 +0800

    version1

---
+++ Project51/Source/Project51/Project51GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project51GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT51_API AProject51GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
